171d15af0fd04106bcd9e002366ea85e0b51d3b9
rarar <me@rarar.com>
2020-04-04 07:45:11 -0700

    updated bot2bot project

--- routes/conversation.js
+++ routes/conversation.js
@@ -84,15 +84,21 @@ const conversationexperiments = [
     type: "Art Research",
     url: "/conversation/bot2bot",
     desc: "What happens when you place one conversational agent against another and exposing the dialogue?",
-    img: "/images/placeholder.png"
+    img: "/images/bot2bot-main.png"
   }
 ];
 
 
 
-router.get('/map', function(req, res, next) {
-
-  res.render('project');
+router.get('/bot2bot', function(req, res, next) {
+  res.locals.h1 = "bot2bot";
+  res.locals.desc = "What happens when you place one conversational agent against another and exposing the dialogue?";
+  res.locals.videothumb = '<div style="padding:68.88% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/404026366?loop=1&title=0&byline=0&portrait=0" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>';
+  res.locals.deets = "<h2>⚒️️ Medium</h2><ul><li>net art</li></ul><h2>🕒 Year</h2><ul><li>2017–2019</li></ul>";
+  res.locals.concept = "<p>bot2bot is an installation that shifts the perspective of human engagement from that of participant to observer. Through this vantage point, participants eavesdrop on a conversation occurring between two conversational agents. In effect, the ensuing conversation can be logical but often ends up in nonsensical tangents and infinite loops of misunderstanding. The work serves to highlight the challenges in state-of-the-art voice-based agents.</p><p>Truly understanding how to hold conversation is a part of life that feels second nature. Regardless of conversational aptitude, most humans are able to hold a basic conversation and at least have small talk with one another.</p><p>Despite its seemingly mundane qualities, conversation is complex and can be a delicate dance between two or more individuals. The study of conversational analysis is a testament to the complexity at which humanity operates. Its literature shows that humans in practice are not as eloquent or clear as dialog in literature or modern-day cinema, yet mutual understanding is still achieved.</p><p>How do we embed a sense of conversational understanding and capability within computational systems?</p><p>bot2bot leverages the Natural Conversation Framework (NCF) developed at IBM Research - Almaden to place one agent in conversation with another. This framework takes a more holistic and human approach in the design of turn-based interactions by utilizing the formal field of Conversation Analysis (CA) (Sacks, Schegloff, Jefferson, 1974). CA studies the manner in which humans take turns and sequentially organize conversation. In leveraging NCF, bot2bot elucidates the challenges of mimicking human conversation, since its nuanced components are difficult to replicate. The work not only serves an aesthetic purpose but is now being used to test for the humanness of conversation and the parts the get lost in translation.</p>";
+  res.locals.content = '<section class="references"><h3>References</h3><ul><li>Sacks, Harvey, Schegloff, Emanuel A., & Jefferson, Gail (1974). A simplest systematics for the organization of turn-taking for conversation. Language, 50, 696–735</li></ul></section>';
+  res.locals.title = 'Raphael Arar';
+  res.render('art');
 });
 
 
